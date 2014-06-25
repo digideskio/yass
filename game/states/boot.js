6b3508949c9867e2c80fc0dea9d445e746ceffef
Indiana <piotr.jakubowski@gmail.com>
2014-06-25 14:42:05 +0200

    some clearing

--- game/states/boot.js
+++ game/states/boot.js
@@ -11,33 +11,6 @@ Boot.prototype = {
   },
   create: function() {
     this.game.input.maxPointers = 1;
-//    this.stage.disableVisibilityChange = true;
-//    if (this.game.device.desktop)
-//    {
-//        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-//        this.scale.minWidth = 480;
-//        this.scale.minHeight = 260;
-//        this.scale.maxWidth = 1024;
-//        this.scale.maxHeight = 768;
-//        this.scale.pageAlignHorizontally = true;
-//        this.scale.pageAlignVertically = true;
-//        this.scale.setScreenSize(true);
-//    }
-//    else
-//    {
-//        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-//        this.scale.minWidth = 480;
-//        this.scale.minHeight = 260;
-//        this.scale.maxWidth = 1024;
-//        this.scale.maxHeight = 768;
-//        this.scale.pageAlignHorizontally = true;
-//        this.scale.pageAlignVertically = true;
-//        this.scale.forceOrientation(true, false);
-//        this.scale.hasResized.add(this.gameResized, this);
-////        this.scale.enterIncorrectOrientation.add(this.enterIncorrectOrientation, this);
-////        this.scale.leaveIncorrectOrientation.add(this.leaveIncorrectOrientation, this);
-//        this.scale.setScreenSize(true);
-//    }    
     this.game.state.start('preload');
   }
 };
